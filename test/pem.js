73ad918c32cb03e4fe2184fe7cd57b599241f98b
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-07-11 22:22:23 +0200

    Update lib/jwk: Parse PEM data to Buffers, not Base64URL

--- test/pem.js
+++ test/pem.js
@@ -17,16 +17,16 @@ suite( 'JSON Web Key', function() {
 
   test( 'parse a PEM encoded public key', function() {
     var key = JSONWebKey.fromPEM( keys.rsa.public )
-    assert.deepEqual( key, {
+    assert.deepEqual( key, JSONWebKey.fromJSON({
       kty: 'RSA',
       n: 'oL9U7lsMfBGZiFO_NmvTbPlPaMgMfg9iuxO2IkgKrJbKVtrGvfzNCOMIaO_wAx8AIf3-tegeaEWWV6FyO6haW1zPhKovVAYyXQKof8CKvueooTie46d0JAHirdAGWn2BWCQKQ-GlFqqMx2ou1BHv9MxfGKaT9CjT8cIROl1ptag3kdUH5ZsjhGmdg_TNXeu4wtiYVf0JG9nWfZncX4Dgv6IpSCoQiGf6FIE_q0jaUhpdBdQ6HEL_s6O3L45FFYvGfAuiciuKVZugR3hXCUJ26NmShMKfdu5qUKPQ02-IQAFGncnMNOVPeDhkLMMIaNerGCsjVz1l_TjXOSTW-h1paw',
       e: 'AQAB',
-    })
+    }))
   })
 
   test( 'parse a PEM encoded private key', function() {
     var key = JSONWebKey.fromPEM( keys.rsa.private )
-    assert.deepEqual( key, {
+    assert.deepEqual( key, JSONWebKey.fromJSON({
       kty: 'RSA',
       n: 'oL9U7lsMfBGZiFO_NmvTbPlPaMgMfg9iuxO2IkgKrJbKVtrGvfzNCOMIaO_wAx8AIf3-tegeaEWWV6FyO6haW1zPhKovVAYyXQKof8CKvueooTie46d0JAHirdAGWn2BWCQKQ-GlFqqMx2ou1BHv9MxfGKaT9CjT8cIROl1ptag3kdUH5ZsjhGmdg_TNXeu4wtiYVf0JG9nWfZncX4Dgv6IpSCoQiGf6FIE_q0jaUhpdBdQ6HEL_s6O3L45FFYvGfAuiciuKVZugR3hXCUJ26NmShMKfdu5qUKPQ02-IQAFGncnMNOVPeDhkLMMIaNerGCsjVz1l_TjXOSTW-h1paw',
       e: 'AQAB',
@@ -36,7 +36,7 @@ suite( 'JSON Web Key', function() {
       dp: 'pqVsWwddipxEXB3iPjQbwtaWv15vqwIS7g2GkdlJOOCmzBToeZ5NgwmLcqPBj3SNWeL2dmFnW6Ngl_BQdr6OQRW0_rjY5odZ3vVya3RzTwCZtrUhhstHzC5kWvmr4eiZq1eN0l4JLQaj062dJVWtAp-uHiwY1zEhqhvp7ffUEgs',
       dq: 'p_0VXmJUrB9b2s3r51qRM-160VCxVjML6SMBOZ37CJqpKeY0HX2v_H0XhASJ57Zvj8JPWW7sA8WuMXo-ofO9Gmddkg8UWcjRMnDuzIdQkLBVfqVhinWeJUEVIZbLKo9l-Rm-QPnxeKMsAzCx-8Pr4kQijFkBq1NsZM-VEhVPrIU',
       qi: 'yyMAH5h3ag8-RocuOa7Iu7yUFGgr2OJssYbZDchjug5jptne6LNOfQ8uPFhOv6YoHg0bm6yNQ50zr35lipnEpD8l__H_DoH3mEGlJ9DgUO_lTwI6kNoswdMtNy8fg5NiSV876OAob2c_REj2Ok42Cz2XY3iNMk6jddqV8eDfBvY',
-    })
+    }))
   })
 
 })
