e20f69f5878dfa97b88e89e92ff5906c6c4ff078
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-07-11 22:54:38 +0200

    Update test/jwk: Reorder, group by key algorithm

--- test/jwk.js
+++ test/jwk.js
@@ -4,17 +4,6 @@ var JSONWebKey = require( '..' )
 
 suite( 'JSON Web Key', function() {
 
-  test( 'parse JWK encoded EC public key', function() {
-
-    var jwk = require( './data/a1-public-keys' )
-    var key = JSONWebKey.fromJSON( jwk.keys[0] )
-
-    assert.ok( key instanceof JSONWebKey )
-    assert.deepEqual( Object.keys( key ), Object.keys( jwk.keys[0] ) )
-    assert.deepEqual( key.toJSON(), jwk.keys[0] )
-
-  })
-
   test( 'parse JWK encoded RSA public key', function() {
 
     var jwk = require( './data/a1-public-keys' )
@@ -26,17 +15,6 @@ suite( 'JSON Web Key', function() {
 
   })
 
-  test( 'parse JWK encoded EC private key', function() {
-
-    var jwk = require( './data/a2-private-keys' )
-    var key = JSONWebKey.fromJSON( jwk.keys[0] )
-
-    assert.ok( key instanceof JSONWebKey )
-    assert.deepEqual( Object.keys( key ), Object.keys( jwk.keys[0] ) )
-    assert.deepEqual( key.toJSON(), jwk.keys[0] )
-
-  })
-
   test( 'parse JWK encoded RSA private key', function() {
 
     var jwk = require( './data/a2-private-keys' )
@@ -45,6 +23,28 @@ suite( 'JSON Web Key', function() {
     assert.ok( key instanceof JSONWebKey )
     assert.deepEqual( Object.keys( key ), Object.keys( jwk.keys[1] ) )
     assert.deepEqual( key.toJSON(), jwk.keys[1] )
+
+  })
+
+  test( 'parse JWK encoded EC public key', function() {
+
+    var jwk = require( './data/a1-public-keys' )
+    var key = JSONWebKey.fromJSON( jwk.keys[0] )
+
+    assert.ok( key instanceof JSONWebKey )
+    assert.deepEqual( Object.keys( key ), Object.keys( jwk.keys[0] ) )
+    assert.deepEqual( key.toJSON(), jwk.keys[0] )
+
+  })
+
+  test( 'parse JWK encoded EC private key', function() {
+
+    var jwk = require( './data/a2-private-keys' )
+    var key = JSONWebKey.fromJSON( jwk.keys[0] )
+
+    assert.ok( key instanceof JSONWebKey )
+    assert.deepEqual( Object.keys( key ), Object.keys( jwk.keys[0] ) )
+    assert.deepEqual( key.toJSON(), jwk.keys[0] )
 
   })
 
