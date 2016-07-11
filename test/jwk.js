82894aa0350594fb6421066a468777c24310bfa1
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-07-11 22:06:17 +0200

    Update test/jwk: Add assertions

--- test/jwk.js
+++ test/jwk.js
@@ -5,27 +5,47 @@ var JSONWebKey = require( '..' )
 suite( 'JSON Web Key', function() {
 
   test( 'parse JWK encoded EC public key', function() {
+
     var jwk = require( './data/a1-public-keys' )
     var key = JSONWebKey.fromJSON( jwk.keys[0] )
-    console.log( key )
+
+    assert.ok( key instanceof JSONWebKey )
+    assert.deepEqual( Object.keys( key ), Object.keys( jwk.keys[0] ) )
+    assert.deepEqual( key.toJSON(), jwk.keys[0] )
+
   })
 
   test( 'parse JWK encoded RSA public key', function() {
+
     var jwk = require( './data/a1-public-keys' )
     var key = JSONWebKey.fromJSON( jwk.keys[1] )
-    console.log( key )
+
+    assert.ok( key instanceof JSONWebKey )
+    assert.deepEqual( Object.keys( key ), Object.keys( jwk.keys[1] ) )
+    assert.deepEqual( key.toJSON(), jwk.keys[1] )
+
   })
 
   test( 'parse JWK encoded EC private key', function() {
+
     var jwk = require( './data/a2-private-keys' )
     var key = JSONWebKey.fromJSON( jwk.keys[0] )
-    console.log( key )
+
+    assert.ok( key instanceof JSONWebKey )
+    assert.deepEqual( Object.keys( key ), Object.keys( jwk.keys[0] ) )
+    assert.deepEqual( key.toJSON(), jwk.keys[0] )
+
   })
 
   test( 'parse JWK encoded RSA private key', function() {
+
     var jwk = require( './data/a2-private-keys' )
     var key = JSONWebKey.fromJSON( jwk.keys[1] )
-    console.log( key )
+
+    assert.ok( key instanceof JSONWebKey )
+    assert.deepEqual( Object.keys( key ), Object.keys( jwk.keys[1] ) )
+    assert.deepEqual( key.toJSON(), jwk.keys[1] )
+
   })
 
 })
